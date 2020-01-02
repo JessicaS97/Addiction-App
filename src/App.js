66ce6fa1fda09afc1a37cc6d4cfe144b4f66d836
Jessica J Sun <jessi.slm@hotmail.com>
2020-01-03 00:32:48 +1100

    Changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Footer from './Components/Footer'
+import Header from './Components/Footer'
+import MainContent from './Components/Footer'
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <MainContent />
+        <Footer />
+      </div>
+    ); 
+  }
 }
 
-export default App;
+export default App
