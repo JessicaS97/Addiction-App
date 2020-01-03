f56cb693d4e37f02319c73db0e425c807e6e4a74
Jessica J Sun <jessi.slm@hotmail.com>
2020-01-03 11:00:34 +1100

    Header changes

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,13 +1,17 @@
 import React from "react"
+import './NavBar.css'
 
 class NavBar extends React.Component {
     render() {
         return(
-            <header id="navbar">
-                <nav>
+            <header className="navbar">
+                <div className="navbar-top"></div>
+                <nav className="navbar-navigation">
                     <div></div>
-                    <div><a href="/">LOGO</a></div>
-                    <div>
+                    <div className="navbar-logo">
+                        <a href="/"><img src="logo.png" alt="logo"></img></a>
+                    </div>
+                    <div className="navbar-items">
                         <ul>
                             <li><a href="/">About</a></li>
                             <li><a href="/">FAQs</a></li>
