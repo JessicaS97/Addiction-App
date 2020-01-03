37c87f2f14b8d3ea7c49155863b2e7f16dd06802
Jessica J Sun <jessi.slm@hotmail.com>
2020-01-03 14:08:30 +1100

    NavBar and header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React from 'react'
+
 import Footer from './Components/Footer'
 import Header from './Components/Header'
 import MainContent from './Components/MainContent/MainContent'
+import SideBar from './Components/SideBar/SideBar'
+import Backdrop from './Components/Backdrop/Backdrop'
 
 class App extends React.Component {
+
+  state = {
+      sideBarOpen: false
+  }
+
+  toggleClickHandler = () => {
+    this.setState(prevState => {
+      return {sideBarOpen: !prevState.sideBarOpen}
+    });
+  }
+
   render() {
+    let sideBar, backdrop
+    if (this.state.sideBarOpen) {
+      sideBar = <SideBar/ >
+      backdrop = <Backdrop />
+    }
     return (
-      <div className="App">
+      <div className="App" style={{height:'100%'}}>
         <Header />
+        {sideBar}
+        {backdrop}
         <MainContent />
         <Footer />
       </div>
---
+++ src/Components/Backdrop/Backdrop.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './Backdrop.css'
+
+class Backdrop extends React.Component {
+    render() {
+        return(
+            <div className="backdrop"></div>
+        )
+    }
+}
+
+export default Backdrop
