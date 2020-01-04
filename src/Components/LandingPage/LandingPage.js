acf1b04263ec42429299cb226f44e006a30da16d
Jessica J Sun <jessi.slm@hotmail.com>
2020-01-04 22:45:44 +1100

    Main page modifications

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -1,14 +1,19 @@
 import React from "react"
 
 import './LandingPage.css'
+import background from './meditation.png'
 
 class LandingPage extends React.Component {
     render() {
         return(
             <div className="landingPage">
-                <h1>Image here</h1>
-                <p>QUOTE</p>
+                <div className="background-img">
+                    <img src={background} alt="meditation"></img>
+                </div>
+                <p>Journey to good habits</p>
+                <br></br>
                 <button>Register</button>
+                <br></br>
                 <a href="/">Already have an account?</a>
             </div>
         )
