7be699718aa4f9fc9d8309be258e6009428b574c
Jessica J Sun <jessi.slm@hotmail.com>
2020-01-07 01:12:59 +1100

    Initial log in

--- src/Components/LogIn/LogIn.js
+++ src/Components/LogIn/LogIn.js
@@ -7,6 +7,26 @@ class LogIn extends React.Component {
         return(
             <div className="login">
                 <h1>Welcome Back</h1>
+                <form onSubmit={this.handleSubmit} noValidate>
+                    <div className="userName">
+                        <label htmlFor="userName">User Name</label>
+                        <input 
+                                type="text" 
+                                placeholder="User Name"
+                                name="userName" 
+                                noValidate
+                            />
+                    </div>
+                    <div className="password">
+                    <label htmlFor="password">Password</label>
+                        <input 
+                                type="password" 
+                                placeholder="Password"
+                                name="password" 
+                                noValidate
+                            />
+                    </div>
+                </form>
             </div>
         )
     }
