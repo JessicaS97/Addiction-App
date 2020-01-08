3e231d6829172c7874ccfe1ac41e61fb71dfe796
Jessica J Sun <jessi.slm@hotmail.com>
2020-01-08 22:22:37 +1100

    PHP Database final changes

--- src/Components/Register/register.php
+++ src/Components/Register/register.php
@@ -9,6 +9,35 @@ if (!empty($firstName) || !empty($userName) || !empty($email) || !empty($passwor
     $dbUsername = "root";
     $dbPassword = "";
     $dbName = "users";
+
+    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
+
+    if (mysqli_connect_error()) {
+        die('Connect Error('.mysqli_connect_errno().')'.mysqli_connect_error());
+    } else {
+        $SELECT = "SELECT email From register Where email = ? Limit 1";
+        $INSERT = "INSERT Into register (firstName, userName, email, password) values
+            (?, ?, ?, ?)"
+        $stmt = $conn->prepare($SELECT);
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $stmt->bind_result($email);
+        $stmt->store_result();
+        $rnum = $stmt->num_rows;
+
+        if ($rnum==0) {
+            $stmt->close();
+
+            $stmt = $conn->prepare($INSERT);
+            $stmt->bind_param("ssss", $firstName, $userName, $email, $password);
+            $stmt->execute();
+            echo "New record inserted successfully";
+        } else {
+            echo "Someone already registered using this email";
+        }
+        $stmt->close();
+        $conn->close();
+    }
 } else {
     echo "All field are required";
     die();
