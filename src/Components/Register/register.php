f0563abb6716aa2536c201bdf8c156fe75cb6311
Jessica J Sun <jessi.slm@hotmail.com>
2020-01-08 22:06:35 +1100

    Additional columns to php database

--- src/Components/Register/register.php
+++ src/Components/Register/register.php
@@ -3,4 +3,14 @@ $firstName = $_POST['firstName'];
 $userName = $_POST['userName'];
 $email = $_POST['email'];
 $password = $_POST['password'];
+
+if (!empty($firstName) || !empty($userName) || !empty($email) || !empty($password)) {
+    $host = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbName = "users";
+} else {
+    echo "All field are required";
+    die();
+}
 ?>
