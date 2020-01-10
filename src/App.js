28107d9b2bb5bd4b05876895826de57eb5a5373b
Jessica J Sun <jessi.slm@hotmail.com>
2020-01-10 18:19:13 +1100

    Additional changes to manu bar

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import LandingPage from './Components/LandingPage/LandingPage'
 import Register from './Components/Register/Register'
 import LogIn from './Components/LogIn/LogIn'
 import GuestHome from './Components/GuestHome/GuestHome'
+import Main from './Components/Main/Main'
 
 class App extends React.Component {
 
@@ -25,6 +26,10 @@ class App extends React.Component {
             <Route path="/register" component={Register}/>
             <Route path="/login" component={LogIn}/>
             <Route path="/guest-home" component={GuestHome} />
+            <Route path="/main" component={Main}></Route>
+            <Route path="/main" component={Main}></Route>
+            <Route path="/main" component={Main}></Route>
+            <Route path="/main" component={Main}></Route>
           </Switch>
         </div>
       </Router>
--- src/Components/MenuBar/MenuBar.js
+++ src/Components/MenuBar/MenuBar.js
@@ -8,22 +8,23 @@ import './MenuBar.css'
 class MenuBar extends React.Component {
     render() {
         return(
-            <div className="menu">
-                <div className="menubar-button">
-                    <Link to="/"> 
-                        <button href="/">Exit
-                        </button>
-                    </Link>         
-                </div>               
-                <div className="menubar">
-                    <ul>
-                        <li><FontAwesomeIcon icon={faHome} size="2x" /></li>
-                        <li><FontAwesomeIcon icon={faUsers} size="2x" /></li>
-                        <li><FontAwesomeIcon icon={faFire} size="2x" /></li>
-                        <li><FontAwesomeIcon icon={faQuestionCircle} size="2x" /></li>
-                    </ul>
-                </div>
+           <div className="menu">
+            <div className="menubar-button">
+                <Link to="/"> 
+                    <button href="/">Log Out</button>
+                </Link>         
+            </div>               
+            <div className="menubar">
+                <ul>
+                    <Link to="/main">
+                        <li><a href="/"><FontAwesomeIcon icon={faHome} size="2x" /></a></li>
+                    </Link>
+                    <li><a href="/"><FontAwesomeIcon icon={faUsers} size="2x" /></a></li>
+                    <li><a href="/"><FontAwesomeIcon icon={faFire} size="2x" /></a></li>
+                    <li><a href="/"><FontAwesomeIcon icon={faQuestionCircle} size="2x" /></a></li>
+                </ul>
             </div>
+        </div>
         )
     }
 }
