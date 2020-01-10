f43f8350547633f20f4c91317ea8a67c335aded0
Jessica J Sun <jessi.slm@hotmail.com>
2020-01-10 15:50:08 +1100

    Adding Font Awesome into menu bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
-
 import Header from './Components/Header'
 import LandingPage from './Components/LandingPage/LandingPage'
 import Register from './Components/Register/Register'
--- src/Components/MenuBar/MenuBar.js
+++ src/Components/MenuBar/MenuBar.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faUsers, faUser } from '@fortawesome/free-solid-svg-icons'
 
 class MenuBar extends React.Component {
     render() {
@@ -12,7 +14,9 @@ class MenuBar extends React.Component {
                     </Link>         
                 </div>               
                 <div className="menubar">
-                    
+                    <FontAwesomeIcon icon={faHome} size="3x" />
+                    <FontAwesomeIcon icon={faUsers} size="3x" />
+                    <FontAwesomeIcon icon={faHome} size="3x" />
                 </div>
             </div>
         )
