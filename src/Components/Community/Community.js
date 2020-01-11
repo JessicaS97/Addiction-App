42a41c1b254d35ef9d837f1fcd9277c45b917558
Jessica J Sun <jessi.slm@hotmail.com>
2020-01-11 15:44:51 +1100

    Minor initial improvements and fixes navigation guest home

--- src/Components/Community/Community.js
+++ src/Components/Community/Community.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import './Community.css'
+import MenuBar from './../MenuBar/MenuBar'
 
 class Community extends React.Component {
     render() {
         return(
             <div className="community">
+                <MenuBar />
                 <h1>Community</h1>
             </div>
         )
