1cdb9cec0b8255c4e36e59da7ce651b94b47246e
Jessica J Sun <jessi.slm@hotmail.com>
2020-01-12 22:27:31 +1100

    Guest Home styling

--- src/Components/AddictionItem/AddictionItem.js
+++ src/Components/AddictionItem/AddictionItem.js
@@ -1,16 +1,17 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
 
 import './AddictionItem.css'
 
 class AddictionItem extends React.Component {
 
     clicked = () => {
-        alert("clicked");
+        
     }
 
     render() {
         return(
-            <div className="addiction-item" onClick={() => this.clicked()}>
+            <div className="addiction-item" onClick={this.clicked()}>
                 <h1>Coffee</h1>
             </div>
         )
