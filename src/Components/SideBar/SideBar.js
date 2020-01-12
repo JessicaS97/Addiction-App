e9548ef76d3e87c7b65a3951a1360a88956a29f1
Jessica J Sun <jessi.slm@hotmail.com>
2020-01-12 19:38:58 +1100

    Home button navbar mobile friendly

--- src/Components/SideBar/SideBar.js
+++ src/Components/SideBar/SideBar.js
@@ -14,6 +14,7 @@ class SideBar extends React.Component {
         return(
             <nav className={sideClasses}>
                 <ul>
+                    <li><a href="/">Home</a></li>
                     <li><a href="/">About</a></li>
                     <li><a href="/">FAQs</a></li>
                     <li><a href="/">Contact</a></li>
