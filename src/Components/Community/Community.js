8123e93e0e896eb06d015bd0b09771e96aeff5b0
Jessica J Sun <jessi.slm@hotmail.com>
2020-01-13 14:27:40 +1100

    Minor changes Motivation page

--- src/Components/Community/Community.js
+++ src/Components/Community/Community.js
@@ -2,34 +2,14 @@ import React from 'react'
 
 import './Community.css'
 import MenuBar from './../MenuBar/MenuBar'
-import quoteService from './../../quoteService'
 
 class Community extends React.Component {
-    state = {
-        quotes: []
-    }
-
-    getQuote = () => {
-        quoteService().then(quote => {
-            this.setState({
-                quotes: quote
-            })
-        })
-    }
-
-    componentDidMount() {
-        this.getQuote()
-    }
 
     render() {
         return(
             <div className="community">
                 <MenuBar />
                 <h1>Community</h1>
-                {this.state.quotes.length > 0 &&
-                this.state.quotes.map(
-                ({quote, questionId}) => <h4>{quote}</h4> 
-                )}
             </div>
         )
     }
--- src/Components/Motivation/Motivation.js
+++ src/Components/Motivation/Motivation.js
@@ -2,13 +2,39 @@ import React from 'react'
 
 import './Motivation.css'
 import MenuBar from './../MenuBar/MenuBar'
+import quoteService from './../../quoteService'
 
 class Motivation extends React.Component {
+    state = {
+        quotes: []
+    }
+
+    getQuote = () => {
+        quoteService().then(quote => {
+            this.setState({
+                quotes: quote
+            })
+        })
+    }
+
+    componentDidMount() {
+        this.getQuote()
+    }
+
     render() {
         return(
-            <div className="motivation">
+            <div>
                 <MenuBar />
+                <div className="motivation">
+
+                </div>
                 <h1>Motivation</h1>
+                <div className="quote-grid">
+                    {this.state.quotes.length > 0 &&
+                    this.state.quotes.map(
+                    ({quote, questionId}) => <h2>{quote}</h2> 
+                    )}
+                </div>
             </div>
         )
     }
