e4bac8b9ebd1f4e300444e2d313b3e3da5f56c98
Jessica J Sun <jessi.slm@hotmail.com>
2020-01-13 19:12:13 +1100

    Choose Addiction List

--- src/Components/NewAddiction/AddictionList.js
+++ src/Components/NewAddiction/AddictionList.js
@@ -15,38 +15,20 @@ class AddictionList extends React.Component {
             <div>
                 <h1>Choose Addiction:</h1>
                 <form className="addiction-form">
-                    <label>
-                        <span>Alcohol</span>
-                        <input type="radio" name="alcohol" value="Alcohol"/>
-                    </label>
-                    <label>
-                        <span>Caffeine</span>
-                        <input type="radio" name="caffeine" value="Caffeine"/>
-                    </label>
-                    <label>
-                        <span>Gambling</span>
-                        <input type="radio" name="gambling" value="Gambling"/>
-                    </label>
-                    <label>
-                        <span>Junk Food</span>
-                        <input type="radio" name="junk'food" value="Junk Food"/>
-                    </label>
-                    <label>
-                        <span>Meat and Diary</span>
-                        <input type="radio" name="meat-diary" value="Meat-Diary"/>
-                    </label>
-                    <label>
-                        <span>Social Media</span>
-                    <input type="radio" name="social-media" value="Social Media"/>
-                    </label>
-                    <label>
-                        <span>Sugar</span>
-                        <input type="radio" name="sugar" value="sugar"/> 
-                    </label>
+                <select>
+                    <option>Alcohol</option>
+                    <option>Caffeine</option>
+                    <option>Gambling</option>
+                    <option>Junk Food</option>
+                    <option>Meet and Diary</option>
+                    <option>Social Media</option>
+                    <option>Sugar</option>
+                    <option>Other</option>
+                </select>
                     <label>
                         <span>Other</span>
-                        <input type="radio" name="other" value="other"/>
-                        <input type="text"/>
+                        <input type="radio"/>
+                        <input type="text" name="other" value=""/>
                     </label>
                 </form>
             </div>
