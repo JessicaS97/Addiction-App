cc127ce581aeb35b772d1aef118bfc40f8f30187
Jessica J Sun <jessi.slm@hotmail.com>
2020-01-13 16:06:22 +1100

    Addiction form steps

--- src/Components/NewAddiction/NewAddiction.js
+++ src/Components/NewAddiction/NewAddiction.js
@@ -3,6 +3,19 @@ import React from 'react'
 import MenuBar from './../MenuBar/MenuBar'
 
 class NewAddiction extends React.Component {
+    state = {
+        step: 1,
+        addiction: '',
+        startDate: '',
+        savingCost: '',
+        savingTime: '',
+        motive: ''
+    }
+
+    nextStep = () => {
+        const { step } = this.state
+    }
+
     render() {
         return(
             <div>
