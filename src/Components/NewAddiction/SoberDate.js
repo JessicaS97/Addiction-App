e1acaf7b82cbced48fd6e7842194cc9b07726725
Jessica J Sun <jessi.slm@hotmail.com>
2020-01-14 21:41:25 +1100

    SoberDate page initial

--- src/Components/NewAddiction/SoberDate.js
+++ src/Components/NewAddiction/SoberDate.js
@@ -13,11 +13,14 @@ class SoberDate extends React.Component {
     }
 
     render() {
+        const {nextStep, prevStep, values, handleChange} = this.props
         return(
             <div className="sober-date">
                 <h1>SoberDate</h1>
-                <button onClick={this.back}>Back</button>
-                <button oncClick={this.next}>Next</button>
+                <input type="date" onChange={handleChange('startDate')} value={values.startDate}></input>
+                <button onClick={prevStep}>Back</button>
+                <br></br>
+                <button oncClick={nextStep}>Next</button>
             </div>
         )
     }
