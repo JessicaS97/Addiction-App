8b6e478793eabb8ca201e2c95e77e8fa6116bbe1
Jessica J Sun <jessi.slm@hotmail.com>
2020-01-17 16:02:32 +1100

    Reducers from redux setup

--- src/Components/GuestHome/GuestHome.js
+++ src/Components/GuestHome/GuestHome.js
@@ -1,25 +1,29 @@
 import React from 'react'
 import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
+import {Provider} from 'react-redux'
 
 import './GuestHome.css'
 import MenuBar from './../MenuBar/MenuBar'
 import AddictionItem from './../AddictionItem/AddictionItem'
+import store from './../../store'
 
 class GuestHome extends React.Component {
     render() {
         return(
-            <div className="guest-home">
-                <MenuBar />
-                <div className="addictions-list">
-                    <h1>Addictions</h1>
-                    <div className="addictions">
-                        <Link to="/new-addiction">
-                            <button>+</button>
-                        </Link>
-                        <AddictionItem />
+            <Provider store={store}>
+                <div className="guest-home">
+                    <MenuBar />
+                    <div className="addictions-list">
+                        <h1>Addictions</h1>
+                        <div className="addictions">
+                            <Link to="/new-addiction">
+                                <button>+</button>
+                            </Link>
+                            <AddictionItem />
+                        </div>
                     </div>
                 </div>
-            </div>
+            </Provider>
         )
     }
 }
